1d3fb89f90d49459ce25436d517191b49a95da09
DESKTOP-QBV1IAC\user <arunvs1984@gmail.com>
2023-06-23 11:36:44 +0530

    Intergrating webrtc code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+import { Server as SocketIO } from "socket.io";
+import * as url from 'url';
 
 /**
  * Module dependencies.
@@ -19,6 +21,36 @@ app.set('port', port);
  */
 
 const server = http.createServer(app);
+const io = new SocketIO(server);
+const room = 'room';
+const rootPath = url.fileURLToPath(new URL('.', import.meta.url));
+
+app.get('/streamer', (req, res) => {
+    res.sendFile(rootPath + 'public/streamer.html');
+});
+app.get('/receiver', (req, res) => {
+    res.sendFile(rootPath + 'public/receiver.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-receiver', () => {
+        socket.join(room);
+        socket.on('disconnect', () => {
+            socket.to(room).emit('receiver-disconnected');
+        });
+    });
+
+    socket.on('join-streamer', () => {
+        socket.join(room);
+        socket.on('disconnect', () => {
+            socket.to(room).emit('streamer-disconnected');
+        });
+    });
+
+    socket.on('screen-sharing-stopped', () => {
+        socket.to(room).emit('remove-video');
+    });
+});
 
 /**
  * Listen on provided port, on all network interfaces.
